6252c951867b16980efeb22181b796322cc078db
Moulin Timothée <timomoulin@msn.com>
2026-01-13 09:15:39 +0100

    login

---
+++ src/main/kotlin/org/ldv/todo0/controller/MainController.kt
@@ -0,0 +1,17 @@
+package org.ldv.todo0.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+@Controller
+class MainController {
+
+    @GetMapping("/todoapp/login")
+    fun login(@RequestParam error: Boolean?, model: Model): String {
+        // Ajoute un attribut "error" au modèle si la requête contient une erreur
+        model.addAttribute("error", error == true)
+        return "pagesVisiteur/login"
+    }
+
+}

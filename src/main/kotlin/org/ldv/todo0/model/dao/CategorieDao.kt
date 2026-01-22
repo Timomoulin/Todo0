2eb5971707da8c1d9afbd437dbd4d5cfe60ea218
Moulin Timothée <timomoulin@msn.com>
2026-01-23 00:08:04 +0100

    final

--- src/main/kotlin/org/ldv/todo0/model/dao/CategorieDao.kt
+++ src/main/kotlin/org/ldv/todo0/model/dao/CategorieDao.kt
@@ -2,6 +2,14 @@ package org.ldv.todo0.model.dao
 
 import org.ldv.todo0.model.entity.Categorie
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface CategorieDao : JpaRepository<Categorie, Long> {
+//Utilise le DSL
+    fun findByNom(nom: String): Categorie?
+
+//Utilise la syntaxe JPQL
+    @Query("select c from Categorie c where upper(c.couleur) = upper(?1)")
+    fun filtreParCouleur(couleur: String): List<Categorie>
+
 }
